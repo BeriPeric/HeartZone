c8a9ff13235fd0c7612b7b3693806ff4c85bbb3e
selenachiang <cc83@students.uwf.edu>
2024-03-07 23:25:37 -0600

    Fixed layout on some pages

--- heart-zone/src/index.js
+++ heart-zone/src/index.js
@@ -4,8 +4,8 @@ import './index.css';
 //import LaunchPage from './launchpage/LaunchPage';
 //import AccountCreationPage from './AccountCreationPage/AccountCreationPage';
 //import ProfileCreation from './ProfileCreation/ProfileCreation';
-import MainMatching from './MainMatching/MainMatching';
-//import Chats from './Chats/Chats';
+//import MainMatching from './MainMatching/MainMatching';
+import Chats from './Chats/Chats';
 //import ChatRoom from './ChatRoom/ChatRoom';
 //import LoginPage from './LoginPage/LoginPage';
 //import Matches from './Matches/Matches';
@@ -18,7 +18,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <MainMatching />
+    <Chats />
   </React.StrictMode>
 );
 
