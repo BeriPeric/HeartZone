779d12c5b5288ace925f2c90d14dac06a3c0c406
Dakota Bell <dcb51@students.uwf.edu>
2024-04-23 17:08:48 -0500

    Updated ProfileView test import pathing.

--- heart-zone/src/tests/ProfileView.test.js
+++ heart-zone/src/tests/ProfileView.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import ProfileView from './ProfileView';
+import ProfileView from '../dirs/ProfileView';
 
 test('renders learn react link', () => {
   render(<ProfileView />);
